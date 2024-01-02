eb63927d5e2b18343030bec346b5753ae475b44a
strus <maciek.struski@gmail.com>
2024-01-02 17:24:22 +0100

    7/12 testów przechodzi

--- zadanie2/src/ParallelEmployer.java
+++ zadanie2/src/ParallelEmployer.java
@@ -3,24 +3,20 @@ import java.util.*;
 
 public class ParallelEmployer implements Employer {
     private final List<Location> visited = new ArrayList<>();
-    private final HashMap<Integer, Object> threads = new HashMap<>();
-    private final HashMap<Integer, Result> threadsResults = new HashMap<>();
+    private final HashMap<Integer, Object> orders = new HashMap<>();
+    private final HashMap<Integer, Result> results = new HashMap<>();
     private OrderInterface order;
     private Location exit = new Location(-1, -1);
-    private final Object lock = new Object();
     private volatile boolean exitFound = false;
 
     @Override
     public void setOrderInterface(OrderInterface order) {
         this.order = order;
-        this.order.setResultListener(new ResultListener() {
-            @Override
-            public void result(Result result) {
-                int orderID = result.orderID();
-                synchronized (threads.get(orderID)) {
-                    threadsResults.put(orderID, result);
-                    threads.get(orderID).notify();
-                }
+        this.order.setResultListener(result -> {
+            int orderID = result.orderID();
+            synchronized (orders.get(orderID)) {
+                results.put(orderID, result);
+                orders.get(orderID).notify();
             }
         });
     }
@@ -29,64 +25,66 @@ public class ParallelEmployer implements Employer {
     public Location findExit(Location startLocation, List<Direction> allowedDirections) {
         visited.add(startLocation);
         findExitRecurrent(startLocation, allowedDirections);
-//        synchronized (lock) {
-//            try {
-//                lock.wait();
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-            return exit;
-//        }
+        return exit;
     }
 
     private void findExitRecurrent(Location startLocation, List<Direction> allowedDirections) {
-        for (Direction allowedDirection : allowedDirections) {
-            if (exitFound) {
-                return;
-            }
+        List<Thread> threads = new ArrayList<>();
+
+        for (var allowedDirection : allowedDirections) {
+            if (exitFound) { return; }
+
             Thread t = new Thread(() -> {
                 Location nextStep = allowedDirection.step(startLocation);
                 int orderID = 0;
                 synchronized (visited) {
                     orderID = orderFindingExit(nextStep, orderID);
                 }
-                if (threads.containsKey(orderID)) {
-                    synchronized (threads.get(orderID)) {
-                        try {
-                            threads.get(orderID).wait();
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        Result res = threadsResults.get(orderID);;
+                if (orders.containsKey(orderID)) {
+                    synchronized (orders.get(orderID)) {
+                        Result res = getResult(orderID);
                         if (res.type() == LocationType.EXIT) {
                             exit = nextStep;
                             exitFound = true;
-//                            synchronized (lock) {
-//                                lock.notify();
-//                            }
-
                         } else {
                             findExitRecurrent(nextStep, res.allowedDirections());
                         }
                     }
                 }
             });
+
             t.start();
-            
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            threads.add(t);
         }
+
+        joinThreads(threads);
     }
 
     private int orderFindingExit(Location nextStep, int orderID) {
         if (!visited.contains(nextStep)) {
             visited.add(nextStep);
             orderID = order.order(nextStep);
-            threads.put(orderID, new Object());
+            orders.put(orderID, new Object());
         }
         return orderID;
+    }
+
+    private Result getResult(int orderID) {
+        try {
+            orders.get(orderID).wait();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return results.get(orderID);
+    }
+
+    private static void joinThreads(List<Thread> threads) {
+        for (var t : threads) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
