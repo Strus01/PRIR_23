6745afe2294632950060e17ccdbfa39a224498d1
strus <maciek.struski@gmail.com>
2024-01-11 19:39:13 +0100

    refactor, nie tworzy już wątków

--- zadanie2/src/ParallelEmployer.java
+++ zadanie2/src/ParallelEmployer.java
@@ -1,22 +1,30 @@
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ParallelEmployer implements Employer {
     private final List<Location> visited = new ArrayList<>();
-    private final HashMap<Integer, Object> orders = new HashMap<>();
-    private final HashMap<Integer, Result> results = new HashMap<>();
-    private OrderInterface order;
-    private Location exit = new Location(-1, -1);
-    private volatile boolean exitFound = false;
+    private final Map<Integer, Location> orders = new HashMap<>();
+    private OrderInterface orderInterface;
+    private Location exit = null;
 
     @Override
     public void setOrderInterface(OrderInterface order) {
-        this.order = order;
-        this.order.setResultListener(result -> {
-            int orderID = result.orderID();
-            synchronized (orders.get(orderID)) {
-                results.put(orderID, result);
-                orders.get(orderID).notify();
+        orderInterface = order;
+        orderInterface.setResultListener(new ResultListener() {
+            @Override
+            public synchronized void result(Result result) {
+                Location previous = orders.get(result.orderID());
+                if (result.type() == LocationType.EXIT) {
+                    exit = previous;
+                    synchronized (orders) {
+                        orders.notify();
+                    }
+                }
+                if (exit == null) {
+                    orderAllLocations(previous, result.allowedDirections());
+                }
             }
         });
     }
@@ -24,69 +32,23 @@ public class ParallelEmployer implements Employer {
     @Override
     public Location findExit(Location startLocation, List<Direction> allowedDirections) {
         visited.add(startLocation);
-        findExitRecurrent(startLocation, allowedDirections);
+        orderAllLocations(startLocation, allowedDirections);
+        synchronized (orders) {
+            try {
+                orders.wait();
+            } catch (InterruptedException ignored) {
+            }
+        }
         return exit;
     }
 
-    private void findExitRecurrent(Location startLocation, List<Direction> allowedDirections) {
-        List<Thread> threads = new ArrayList<>();
-
-        if (exitFound) { return; }
-
-        for (var allowedDirection : allowedDirections) {
-
-            Thread t = new Thread(() -> {
-                Location nextStep = allowedDirection.step(startLocation);
-                int orderID = orderFindingExit(nextStep);
-
-                if (orders.containsKey(orderID)) {
-                    Result result = getResult(orderID);
-                    if (result.type() == LocationType.EXIT) {
-                        exit = nextStep;
-                        exitFound = true;
-                    } else {
-                        findExitRecurrent(nextStep, result.allowedDirections());
-                    }
-                }
-            });
-
-            if (exitFound) { return; }
-
-            t.start();
-            threads.add(t);
-        }
-        joinThreads(threads);
-    }
-
-    private int orderFindingExit(Location nextStep) {
-        int orderID = 0;
-        synchronized (visited) {
-            if (!visited.contains(nextStep)) {
-                visited.add(nextStep);
-                orderID = order.order(nextStep);
-                orders.put(orderID, new Object());
-            }
-            return orderID;
-        }
-    }
-
-    private Result getResult(int orderID) {
-        synchronized (orders.get(orderID)) {
-            try {
-                orders.get(orderID).wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            return results.get(orderID);
-        }
-    }
-
-    private static void joinThreads(List<Thread> threads) {
-        for (var t : threads) {
-            try {
-                t.join();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+    private void orderAllLocations(Location startLocation, List<Direction> allowedDirections) {
+        for (Direction direction : allowedDirections) {
+            Location location = direction.step(startLocation);
+            if (!visited.contains(location)) {
+                visited.add(location);
+                int orderId = orderInterface.order(location);
+                orders.put(orderId, location);
             }
         }
     }
